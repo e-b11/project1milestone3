b87aa810108190b03731e096cbca12a4f4b858a4
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-11-06 14:13:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 let express = require("express");
-let app = new express();                                                                     
+let app = new express();
+let path = require("path");
+
 app.set("view engine", "ejs");
 
 // set up database connection
 const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db.cngheounmzfp.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
-  port: 3306,
- },
+  client: "mysql",
+  connection: {
+    host: "cakescompany.cfsse0o0k0rj.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "Dishsoap5",
+    database: "cake",
+    port: 3306,
+  },
 });
 
-app.get("/",(req,res) => {
-knex
-.select()
-.from("venues")
-.then((result) => {
-res.render("index", {aConcerts: result});
+app.get("/", (req, res) => {
+  knex
+    .select()
+    .from("types")
+    .then((result) => {
+      console.log(result);
+      res.render("index", { cakeList: result });
+    });
 });
-});
+
 app.listen(3000);
