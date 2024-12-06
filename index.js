3cdf7a9a27c3364627dc64f4f505e34deb859f67
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-12-05 20:40:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    host: "milestone3take5-databaseserver-zhv4ifdv9e6o.cfsse0o0k0rj.us-east-2.rds.amazonaws.com",
+    host: "cakesp2db.cfsse0o0k0rj.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "Dishsoap5",
     database: "cake",
